679ec15e842e0832743f460cf91ed442292ba4c7
Joy Chen <t-joychen@expedia.com>
2018-07-13 14:12:39 -0500

    Add more complex antd functionality

---
+++ frontend/config-overrides.js
@@ -0,0 +1,12 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(config, env) {
+    config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);  // change importing css to less
+    config = rewireLess.withLoaderOptions({
+      javascriptEnabled: true,
+      modifyVars: { "@primary-color": "#80CC45" },
+    })(config, env);
+    return config;
+  };
+  
