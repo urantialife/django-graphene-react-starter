48e3a141d1b55ea6ab01672e9c9e1420e697cd62
Joy Chen <t-joychen@expedia.com>
2018-08-16 13:10:03 -0500

    frontend: Add query to HomeView to return ingredients (no client error persists)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { 
+import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
--- frontend/src/components/HomeView.js
+++ frontend/src/components/HomeView.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
 
+const GET_INGREDIENTS = gql`
+{
+    allIngredients {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+}
+`
+
+const Ingredients = () => (
+    <Query query={GET_INGREDIENTS}>
+        {({ loading, error, data }) => {
+        if (loading) return "Loading...";
+        if (error) return `Error! ${error.message}`;
+  
+        return (
+            {data}
+        );
+      }}
+    </Query>
+);
 class HomeView extends Component {
   render() {
     return (
-        <div>hello</div>
+        <div>hello
+            <Ingredients />
+        </div>
     );
   }
 }
